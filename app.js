a4631a57d6fd7e89557ff212b5507ea0c12ce344
BorysLytochkin <borisleen2019@gmail.com>
2024-07-29 15:06:08 +0300

    new html and js

---
+++ app.js
@@ -0,0 +1,54 @@
+"use strict"
+
+let a = 4;
+let b = 20;
+
+let addition = a + b;
+let subtraction = b - a;
+let multiplication = a * b;
+let division = b / a;
+let squared = a ** 2;
+let squareroot = Math.sqrt(b);
+console.log(addition, subtraction, multiplication, division, squared, squareroot);
+
+
+let value;
+//num to str
+value = String(10);
+console.log(value);
+console.log(typeof value);
+
+let value2;
+//boolean to str
+value2 = String(true);
+console.log(value2);
+console.log(typeof value2);
+
+let value3;
+let value4;
+//str to num
+value3 = "6" / 3;
+value4 = Number("25")
+console.log(value3);
+console.log(typeof value3);
+console.log(value4);
+console.log(typeof value4);
+
+let value5;
+//boolean to num
+value5 = Number(true)
+console.log(value5);
+console.log(typeof value5);
+
+let value6;
+//num to boolean 
+value6 = Boolean(10)
+console.log(value6);
+console.log(typeof value6);
+
+let value7;
+//str to boolean 
+value7 = Boolean("Hello")
+console.log(value7);
+console.log(typeof value7);
+
